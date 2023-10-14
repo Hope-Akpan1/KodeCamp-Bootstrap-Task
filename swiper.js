e0f5d80f07c46c3bed972c392663be89977fbeeb
Hope Akpan <134354177+Hope-Akpan1@users.noreply.github.com>
2023-10-15 00:50:57 +0100

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -18,22 +18,5 @@ const swiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    }
-  }
+  
 });
