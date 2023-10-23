56eed30dfee7f6924371767e6029dd5f82ebdfb8
Hope Akpan <134354177+Hope-Akpan1@users.noreply.github.com>
2023-10-23 22:07:51 +0100

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -2,21 +2,51 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  speed: 3000,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  autoplay: {
+    delay: 2000,
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  mousewheel: {
+    invert: true,
   },
+
   
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 18
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 25
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 2000px
+    2000: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+
+  }
 });
